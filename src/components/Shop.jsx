78e358d381ed2c90d23a896cf06c6bea0118b2a6
matt styles <styles.matt135@gmail.com>
2025-06-18 17:34:30 -0400

    favicon and shop work

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,41 @@
 import "../App.css";
+
 function Shop() {
   return (
     <div className="page">
       <div className="landing">
         <h1>SHOP</h1>
-        <p>coming soon</p>
+
+        <div className="product">
+          <div className="product-header">
+            BOOK 1: FRANKIE LANGLEY and the Inter-Galactic Post-Office – $15
+          </div>
+
+          <form
+            action="https://www.paypal.com/cgi-bin/webscr"
+            method="post"
+            target="_blank"
+          >
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input
+              type="hidden"
+              name="hosted_button_id"
+              value="YOUR_BUTTON_ID_HERE"
+            />
+            <input
+              type="image"
+              src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
+              name="submit"
+              alt="Buy Now with PayPal"
+            />
+            <img
+              alt=""
+              src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
+              width="1"
+              height="1"
+            />
+          </form>
+        </div>
       </div>
     </div>
   );
