8c763d7f2e3b1769d86418751d0284a8318f3f25
matt styles <styles.matt135@gmail.com>
2025-06-19 16:45:04 -0400

    Landing updates and css

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,7 +5,7 @@ import meprofile from "../images/me-profile.jpg";
 function Landing() {
   return (
     <div className="page">
-      <div className="landing-header" >FRANKIE LANGLEY.com</div>
+      <div className="landing-header">OUR STORY</div>
       <div className="landing-wrapper">
         <div className="landing-left">
           <img
@@ -23,9 +23,9 @@ function Landing() {
         <div className="landing-right">
           <h2>Meet Matt and Andy</h2>
           <img src={meprofile} alt="Me Profile" className="about-image" />
-          <div className="about-text">
-            <p></p>
-          </div>
+          <p>
+            Andy Gordon and Matt Styles independently produced their dream comic in their home of Knoxville, TN. The southern artists are determined to tackle themes releveant to their home. FRANKIE LANGLEY is a three part graphic novel series that incorporates their passions into a focused exploration of workers' rights and reslience in the face of corruption. We hope you love it! 
+          </p>
         </div>
       </div>
     </div>
