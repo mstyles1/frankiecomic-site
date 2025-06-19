fd30045312faaff5177d5b29fd7a967ceee98266
matt styles <styles.matt135@gmail.com>
2025-06-19 17:08:41 -0400

    Second Deploy before con-- css padding stuff

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,19 +13,24 @@ function Landing() {
             alt="Cover - Frankie Langley Book 1 - Landing"
             className="cover-image"
           />
-          <p>
+          <div className="landing-text">
             An aging mail courier enrolls in a mentor program to earn her long
             deserved freedom from the devestating drenches of intergalactic
             postal work... Read the action-packed sci fi adventure
             <Link to="/shop"> today!</Link>
-          </p>
+          </div>
         </div>
         <div className="landing-right">
           <h2>Meet Matt and Andy</h2>
           <img src={meprofile} alt="Me Profile" className="about-image" />
-          <p>
-            Andy Gordon and Matt Styles independently produced their dream comic in their home of Knoxville, TN. The southern artists are determined to tackle themes releveant to their home. FRANKIE LANGLEY is a three part graphic novel series that incorporates their passions into a focused exploration of workers' rights and reslience in the face of corruption. We hope you love it! 
-          </p>
+          <div className="landing-text">
+            Andy Gordon and Matt Styles independently produced their dream comic
+            in their home of Knoxville, TN. The southern-eastern artists are
+            determined to tackle themes releveant to their home. FRANKIE LANGLEY
+            is a three part graphic novel series that incorporates their
+            passions into a focused exploration of workers' rights and reslience
+            in the face of corruption. Hope ya'll love it!
+          </div>
         </div>
       </div>
     </div>
