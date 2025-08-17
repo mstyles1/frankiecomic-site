e338e8452a70160e1226fedd6401da249c618c24
matt styles <styles.matt135@gmail.com>
2025-08-17 07:43:15 -0700

    Rework landing, new colors and imgs

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,35 +1,45 @@
 import { Link } from "react-router-dom";
 import "../App.css";
-import book1cover from "../images/book1cover.png";
-import meprofile from "../images/me-profile.jpg";
+import frankiestandardcover from "../images/frankie-standard-cover.jpg";
+import andyandme from "../images/andy-and-me.jpg";
+
 function Landing() {
   return (
     <div className="page">
       <div className="landing-header">OUR STORY</div>
+
       <div className="landing-wrapper">
-        <div className="landing-left">
-          <img
-            src={book1cover}
-            alt="Cover - Frankie Langley Book 1 - Landing"
-            className="cover-image"
-          />
+        {/* Left Section */}
+        <div className="landing-box">
+          <div className="image-container">
+            <img
+              src={frankiestandardcover}
+              alt="Cover - Frankie Langley Book 1 - Landing"
+              className="cover-image"
+            />
+            <Link to="/shop" className="overlay-button">
+              Book 1 in the Frankie Langley series available now!
+            </Link>
+          </div>
           <div className="landing-text">
             An aging mail courier enrolls in a mentor program to earn her long
-            deserved freedom from the devestating drenches of intergalactic
-            postal work... Read the action-packed sci fi adventure
+            deserved freedom from the devastating drenches of intergalactic
+            postal work... Read the action-packed sci-fi adventure
             <Link to="/shop"> today!</Link>
           </div>
         </div>
-        <div className="landing-right">
+
+        {/* Right Section */}
+        <div className="landing-box">
           <h2>Meet Matt and Andy</h2>
-          <img src={meprofile} alt="Me Profile" className="about-image" />
+          <img src={andyandme} alt="Me Profile" className="about-image" />
           <div className="landing-text">
             Andy Gordon and Matt Styles independently produced their dream comic
-            in their home of Knoxville, TN. The southern-eastern artists are
-            determined to tackle themes releveant to their home. FRANKIE LANGLEY
-            is a three part graphic novel series that incorporates their
-            passions into a focused exploration of workers' rights and reslience
-            in the face of corruption. Hope ya'll love it!
+            in their home of Knoxville, TN. The southeastern artists are
+            determined to tackle themes relevant to their home. FRANKIE LANGLEY
+            is a three-part graphic novel series that incorporates their
+            passions into a focused exploration of workers' rights and
+            resilience in the face of corruption. Hope y'all love it!
           </div>
         </div>
       </div>
