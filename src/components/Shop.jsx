f85f670c4ab75c151a439088f34afb7080fb0de0
matt styles <styles.matt135@gmail.com>
2025-08-20 16:05:10 -0400

    shop updates

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,6 @@
 import "../App.css";
 import book1cover from "../images/frankie-shop-cover.jpg";
+import bundleimage from "../images/frankie-shop-bundle.jpg";
 
 function Shop() {
   return (
@@ -18,63 +19,71 @@ function Shop() {
                 alt="Cover - Frankie Langley Book 1"
                 className="product-image"
               />
-              <div className="product-header">
-                $15 -- FRANKIE LANGLEY and the Inter-Galactic Post Office --
-                BOOK 1
+              <div className="product-details">
+                <div className="product-header">
+                  FRANKIE LANGLEY and the Inter-Galactic Post Office -- BOOK
+                  1
+                </div>
+                <div className="product-purchase">
+                  <span className="price">$15</span>
+                  <form
+                    action="https://www.paypal.com/cgi-bin/webscr"
+                    method="post"
+                    target="_top"
+                  >
+                    <input type="hidden" name="cmd" value="_s-xclick" />
+                    <input
+                      type="hidden"
+                      name="hosted_button_id"
+                      value="R9GWRPMEH5FZQ"
+                    />
+                    <input type="hidden" name="currency_code" value="USD" />
+                    <input
+                      type="image"
+                      src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif"
+                      name="submit"
+                      alt="Buy Now"
+                    />
+                  </form>
+                </div>
               </div>
-              <form
-                action="https://www.paypal.com/cgi-bin/webscr"
-                method="post"
-                target="_top"
-              >
-                <input type="hidden" name="cmd" value="_s-xclick" />
-                <input
-                  type="hidden"
-                  name="hosted_button_id"
-                  value="R9GWRPMEH5FZQ"
-                />
-                <input type="hidden" name="currency_code" value="USD" />
-                <input
-                  type="image"
-                  src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif"
-                  name="submit"
-                  alt="Buy Now"
-                />
-              </form>
             </div>
 
             {/* Book 1 Bundle */}
             <div className="product">
               <img
-                src={book1cover}
-                alt="Cover - Frankie Langley Book 1"
+                src={bundleimage}
+                alt="Cover - Frankie Langley Book 1 Bundle"
                 className="product-image"
               />
-              <div className="product-header">
-                $50 -- FRANKIE LANGLEY and the Inter-Galactic Post Office --
-                BOOK 1 SHOP BUNDLE (5 BOOKS)
+              <div className="product-details">
+                <div className="product-header">
+                  FRANKIE LANGLEY and the Inter-Galactic Post Office -- BOOK
+                  1 SHOP BUNDLE (5 BOOKS)
+                </div>
+                <div className="product-purchase">
+                  <span className="price">$50</span>
+                  <form
+                    action="https://www.paypal.com/cgi-bin/webscr"
+                    method="post"
+                    target="_blank"
+                  >
+                    <input type="hidden" name="cmd" value="_s-xclick" />
+                    <input
+                      type="hidden"
+                      name="hosted_button_id"
+                      value="NYE566JVFCNNL"
+                    />
+                    <input
+                      type="image"
+                      src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
+                      name="submit"
+                      alt="Buy Now with PayPal"
+                    />
+                  </form>
+                </div>
               </div>
-              <form
-                action="https://www.paypal.com/cgi-bin/webscr"
-                method="post"
-                target="_blank"
-              >
-                <input type="hidden" name="cmd" value="_s-xclick" />
-                <input
-                  type="hidden"
-                  name="hosted_button_id"
-                  value="NYE566JVFCNNL"
-                />
-                <input
-                  type="image"
-                  src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
-                  name="submit"
-                  alt="Buy Now with PayPal"
-                />
-              </form>
             </div>
-
-            {/* More products can go here... */}
           </div>
         </div>
       </div>
