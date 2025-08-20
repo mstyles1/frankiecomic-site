de9937260a81ffcdc2bcf6abb1fa4a0272ad0b96
matt styles <styles.matt135@gmail.com>
2025-08-20 14:06:45 -0400

    Shop fixes

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,5 @@
 import "../App.css";
-import book1cover from "../images/book1cover.png";
+import book1cover from "../images/frankie-shop-cover.jpg";
 
 function Shop() {
   return (
@@ -7,76 +7,75 @@ function Shop() {
       <div className="landing">
         <h1>SHOP</h1>
 
-        <div className="product">
-          {/* Book 1 Single Issue */}
-          <img
-            src={book1cover}
-            alt="Cover - Frankie Langley Book 1"
-            className="product-image"
-          />
-          <div className="product-header">
-            $15 -- FRANKIE LANGLEY and the Inter-Galactic Post Office -- BOOK 1
+        {/* Outer box */}
+        <div className="product-box">
+          {/* Grid container */}
+          <div className="product-grid">
+            {/* Book 1 Single Issue */}
+            <div className="product">
+              <img
+                src={book1cover}
+                alt="Cover - Frankie Langley Book 1"
+                className="product-image"
+              />
+              <div className="product-header">
+                $15 -- FRANKIE LANGLEY and the Inter-Galactic Post Office --
+                BOOK 1
+              </div>
+              <form
+                action="https://www.paypal.com/cgi-bin/webscr"
+                method="post"
+                target="_top"
+              >
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input
+                  type="hidden"
+                  name="hosted_button_id"
+                  value="R9GWRPMEH5FZQ"
+                />
+                <input type="hidden" name="currency_code" value="USD" />
+                <input
+                  type="image"
+                  src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif"
+                  name="submit"
+                  alt="Buy Now"
+                />
+              </form>
+            </div>
+
+            {/* Book 1 Bundle */}
+            <div className="product">
+              <img
+                src={book1cover}
+                alt="Cover - Frankie Langley Book 1"
+                className="product-image"
+              />
+              <div className="product-header">
+                $50 -- FRANKIE LANGLEY and the Inter-Galactic Post Office --
+                BOOK 1 SHOP BUNDLE (5 BOOKS)
+              </div>
+              <form
+                action="https://www.paypal.com/cgi-bin/webscr"
+                method="post"
+                target="_blank"
+              >
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input
+                  type="hidden"
+                  name="hosted_button_id"
+                  value="NYE566JVFCNNL"
+                />
+                <input
+                  type="image"
+                  src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
+                  name="submit"
+                  alt="Buy Now with PayPal"
+                />
+              </form>
+            </div>
+
+            {/* More products can go here... */}
           </div>
-
-          <form
-            action="https://www.paypal.com/cgi-bin/webscr"
-            method="post"
-            target="_top"
-          >
-            <input type="hidden" name="cmd" value="_s-xclick" />
-            <input
-              type="hidden"
-              name="hosted_button_id"
-              value="R9GWRPMEH5FZQ"
-            />
-            <input type="hidden" name="currency_code" value="USD" />
-            <input
-              type="image"
-              src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif"
-              border="0"
-              name="submit"
-              title="PayPal - The safer, easier way to pay online!"
-              alt="Buy Now"
-            />
-          </form>
-        </div>
-
-        <div className="product">
-          {/* Book 1 Shop Bundle */}
-          <img
-            src={book1cover}
-            alt="Cover - Frankie Langley Book 1"
-            className="product-image"
-          />
-          <div className="product-header">
-            $50 -- FRANKIE LANGLEY and the Inter-Galactic Post Office -- BOOK 1
-            SHOP BUNDLE (5 BOOKS)
-          </div>
-
-          <form
-            action="https://www.paypal.com/cgi-bin/webscr"
-            method="post"
-            target="_blank"
-          >
-            <input type="hidden" name="cmd" value="_s-xclick" />
-            <input
-              type="hidden"
-              name="hosted_button_id"
-              value="NYE566JVFCNNL"
-            />
-            <input
-              type="image"
-              src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
-              name="submit"
-              alt="Buy Now with PayPal"
-            />
-            <img
-              alt=""
-              src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-              width="1"
-              height="1"
-            />
-          </form>
         </div>
       </div>
     </div>
